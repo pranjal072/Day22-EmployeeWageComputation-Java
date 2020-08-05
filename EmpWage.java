f52c377191d80a2410fbfe618d10e9d6851fe105
pranjal072 <pranjaltyagi97@gmail.com>
2020-08-05 22:30:43 +0530

    Program is an extension of Day 21 Assignment

---
+++ EmpWage.java
@@ -0,0 +1,42 @@
+public class EmpWage
+{
+   public static final int rateperHr = 20;
+   public static final int fullTime = 1;
+   public static final int partTime = 2;
+   public static final int maxDays = 20;
+   public static final int maxHr = 100;
+
+   public static void computeEmpWage()
+   {
+      int workHr,wage;
+      int Salary = 0, totHr = 0, totDays = 0;
+
+      while ( totHr < maxHr && totDays < maxDays )
+      {
+         int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;
+         switch (empCheck)
+         {
+            case fullTime:
+               workHr=8;
+               break;
+            case partTime:
+               workHr=4;
+               break;
+            default:
+               workHr=0;
+         }
+         totDays++;
+         totHr += workHr;
+         wage = workHr * rateperHr;
+         System.out.println("Day "+ totDays + " wage is " + wage);
+         Salary += wage;
+      }
+
+      System.out.println("Total Salary of this Month is : " + Salary);
+   }
+   public static void main(String[] args)
+   {
+      System.out.println("Welcome to Employee Wage Calculation !!");
+      computeEmpWage();
+   }
+}
